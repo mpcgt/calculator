e86e51bfbb084a60f3274ce64aad022f19c02cb6
Max <max.pcgt@gmail.com>
2024-05-17 20:33:28 +0200

    Update file

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,3 +2,14 @@ import { useContext } from "react";
 import { TouchableOpacity, Text } from "react-native";
 import { ThemeContext } from "../context/ThemeContext";
 import { Styles } from "../styles/GlobalStyles";
+
+interface ButtonProps {
+    onPress: () => void;
+    title: string;
+    isBlue?: boolean;
+    isGray?: boolean;
+}
+
+export default function Button({ title, onPress, isBlue, isGray }: ButtonProps) {
+    const theme = useContext(ThemeContext);
+}
