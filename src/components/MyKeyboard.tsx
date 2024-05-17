54c8e2c007191633b21b78c5a011ac8936a00e25
Max <max.pcgt@gmail.com>
2024-05-17 21:00:53 +0200

    Update file

--- src/components/MyKeyboard.tsx
+++ src/components/MyKeyboard.tsx
@@ -5,5 +5,11 @@ import { Styles } from "../styles/GlobalStyles";
 import { myColors } from "../styles/colors";
 
 export default function MyKeyboard() {
-    
+    const [firstNumber, setFirstNumber] = React.useState("")
+    const [secondNumber, setSecondNumber] = React.useState("")
+    const [operation, setOperation] = React.useState("")
+    const [result, setResult] = React.useState<Number | null>(null);
+    return(
+
+    )
 }
