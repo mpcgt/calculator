4f2291d3c4bd8c0ddfa01eeaacc4983d00cfb388
Max <max.pcgt@gmail.com>
2024-05-17 21:09:37 +0200

    Updates files

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import { StyleSheet, Text, View, Switch } from 'react-native';
 import { ThemeContext } from './src/context/ThemeContext';
 import { myColors } from './src/styles/colors';
 import Button from './src/components/Button';
+import MyKeyboard from './src/components/MyKeyboard';
 
 export default function App() {
   const [theme, setTheme] = useState('light');
@@ -15,6 +16,7 @@ export default function App() {
         value={theme === 'light'}
         onValueChange={() => setTheme(theme === 'light' ? 'dark' : 'light')}
       />
+      <MyKeyboard />
     </View>
     </ThemeContext.Provider>
   );
