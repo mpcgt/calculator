ee4d44f69cc31f346559f646570a2086b4e6f8e2
Max <max.pcgt@gmail.com>
2024-05-17 21:07:56 +0200

    Update file

--- src/components/MyKeyboard.tsx
+++ src/components/MyKeyboard.tsx
@@ -11,5 +11,25 @@ export default function MyKeyboard() {
     const [result, setResult] = React.useState<Number | null>(null);
     return(
 
-    )
+    const handleNumberPress = (buttonValue: string) => {
+        if (firstNumber.length < 10) {
+            setFirstNumber(firstNumber + buttonValue);
+    }
+   };
+
+   const handleOperationPress = (buttonValue: string) => {
+    setOperation(buttonValue);
+    setSecondNumber(firstNumber);
+    setFirstNumber("");
+   };
+
+   const clear = () => {
+    setFirstNumber("");
+    setSecondNumber("");
+    setOperation("");
+    setResult(null);
+   };
+
+   return(
+   )
 }
