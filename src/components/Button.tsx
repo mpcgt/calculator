651d9a8f6e3f1d95ac1e1fde0089ece9e0f2a19f
Max <max.pcgt@gmail.com>
2024-05-17 20:42:01 +0200

    Update file

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -12,4 +12,29 @@ interface ButtonProps {
 
 export default function Button({ title, onPress, isBlue, isGray }: ButtonProps) {
     const theme = useContext(ThemeContext);
+    return (
+        <TouchableOpacity
+            style={
+                isBlue
+                ? Styles.btnBlue
+                : isGray
+                ? Styles.btnGray
+                : theme === "light"
+                ? Styles.btnLight
+                : Styles.btnDark
+            }
+            onPress={onPress}>
+            <Text
+                style={
+                    isBlue || isGray
+                    ? Styles.smallTextLight
+                    : theme === "dark"
+                    ? Styles.smallTextLight
+                    : Styles.smallTextDark
+                }
+            >
+                {title}
+            </Text>
+            </TouchableOpacity>
+    )
 }
